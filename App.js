6d81f4ef359cdeca410f4a2731e0d81ba990f93c
SarayuGandla <149779242+SarayuGandla@users.noreply.github.com>
2025-06-19 21:55:08 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,18 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import TextUpdater from './components/TextUpdater';
-import HoverMessage from './components/HoverMessage';
-import FocusBlur from './components/FocusBlur';
+
+import LifecycleDemo from './components/LifecycleDemo';
 
 function App() {
   return (
-     <div className="App">
-     {/* <TextUpdater/> */}
-    {/* <HoverMessage/> */}
-    <FocusBlur/>
+    <div>
+      <LifecycleDemo/>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // App.jsx
+// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+// import AboutUs from './components/AboutUs';
+// import Contact from './components/Contact';
+
+// function App() {
+//   return (
+//     <Router>
+//       <nav style={{ marginBottom: '1rem' }}>
+//         <Link to="/about">About Us</Link> | 
+//         <Link to="/contact/123?source=homepage"> Contact </Link>
+//       </nav>
+
+//       <Routes>
+//         <Route path="/about" element={<AboutUs />} />
+//         <Route path="/contact/:userId" element={<Contact />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
---
+++ LifecycleDemo.js
@@ -0,0 +1,39 @@
+// In LifecycleDemo.js (or LifecycleDemo.jsx)
+import React, { Component } from 'react';
+
+class LifecycleDemo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+    console.log('Constructor');
+  }
+
+  componentDidMount() {
+    console.log('Component Did Mount');
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('Component Did Update');
+  }
+
+  componentWillUnmount() {
+    console.log('Component Will Unmount');
+  }
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    console.log('Render');
+    return (
+      <div>
+        <h2>Class Component</h2>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+      </div>
+    );
+  }
+}
+
+export default LifecycleDemo;  // ✅ Default export
